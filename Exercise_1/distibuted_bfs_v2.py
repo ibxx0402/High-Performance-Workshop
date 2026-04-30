1ea228000f40cda30fcbd092e84420397a8e60f1
fjepse22 <fjepse22@student.aau.dk>
2026-04-30 13:56:32 +0200

    Fixed bugs in parralel version.
    Now with extra overhead. IT can now handle others. Bug was caused by using split size to send mesages instead of the number of processes used.
    Note the program now have even more overhead and the number of vertires modulus number of processers must be 0 because of comm.allgather()

--- Exercise_1/distibuted_bfs_v2.py
+++ Exercise_1/distibuted_bfs_v2.py
@@ -8,17 +8,22 @@ import numpy as np
 # tag 2 = Sending a vertices to check out.
 
 
-def bfsv_parallel(graph, source,processors):
+def bfsv_parallel(graph, source):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank() # often just zero, seems to be "random"
     world_size = comm.Get_size() # How many processes aviable
 
     # TODO Split up the array.
 
+  
+
     
     # Master p is rank 0
     if rank == 0:
         V = len(graph)
+
+        assert V % world_size == 0, f"{V} % {world_size} = {V % world_size} Error the number of processors must be a mutliple of number of vertices"
+
         distance = np.full(V, -1, dtype=np.int32)
         distance[source] = 0
 
@@ -33,11 +38,12 @@ def bfsv_parallel(graph, source,processors):
             split_offset = 1
 
 
+        print(split+split_offset)
+        node_splits = np.full((world_size,2),-1,dtype=np.int32)
+        print(node_splits)
+        print(f"world size = {world_size}")
+        for i in range(0,world_size):
 
-        node_splits = np.full((split+split_offset,2),-1,dtype=np.int32)
-        
-
-        for i in range(0,split-split_offset):
             node_splits[i,0]= i*split
             node_splits[i,1] = (i+1)*split-1
 
@@ -54,6 +60,8 @@ def bfsv_parallel(graph, source,processors):
                 distance_split_tx = distance[i*split:(i+1)*split]
                 frontier_array_tx = frontier_array[i*split:(i+1)*split]
 
+
+                print(f"p{rank} sends data to rank {i}")
                 comm.send([vertex_tx,distance_split_tx,frontier_array_tx,node_splits],dest=i,tag=1) # Tag 1 = 
                 pass
 
@@ -72,8 +80,8 @@ def bfsv_parallel(graph, source,processors):
 
     while True:
         lo_neighbor_array = np.zeros(V, dtype=np.bool_) # The Visited nodes in this itteration
-        lo_to_send = np.full(len(lo_nodes),-1)
-        full_list = np.full(len(graph),-1)
+        lo_to_send = np.full(len(lo_nodes[0])*len(lo_nodes),-1)
+        full_list = np.full(len(graph)*len(graph[0]),-1)
 
         #if rank == 1:
             #print(f"p1 lo frontier = {lo_frontier_array}")
@@ -109,10 +117,9 @@ def bfsv_parallel(graph, source,processors):
 
         comm.barrier
 
+        # A bit of overhead as np.list are send.
         comm.Allgather(lo_to_send,full_list)
-
-
-        # TODO Handle the recieved data.
+    
         for v in full_list:
             if v == -1:
                 pass
@@ -139,6 +146,8 @@ def bfsv_parallel(graph, source,processors):
         comm.barrier()
 
         front_buffer = np.zeros(len(graph),dtype=np.bool)
+
+        # a bit of overhad at 
         comm.Allgather(lo_frontier_array,front_buffer)
 
         if not np.any(front_buffer):
@@ -191,10 +200,10 @@ if __name__ == "__main__":
     graph = np.array([[1,2,4],[3,4,-1],[4,5,-1],[6,-1,-1],[6,7,8],[7,-1,-1],[8,-1,-1],[8,-1,-1],[-1,-1,-1]],dtype=np.int32)
 
     # Nasty graphs not supported. len of graph must be multiple of number of processes
-    nasty_graph = np.array([[1,2,4],[3,4,-1],[4,5,-1],[6,-1,-1],[6,7,8],[7,-1,-1],[8,-1,-1],[8,-1,-1],[-1,-1,-1],[0,-1,-1],[-1,-1,-1],[-1,-1,-1]],dtype=np.int32)
+    nasty_graph = np.array([[1,2,4],[3,4,-1],[4,5,-1],[6,-1,-1],[6,7,8],[7,-1,-1],[8,-1,-1],[8,-1,-1],[-1,-1,9],[10,-1,-1],[11,-1,-1],[-1,-1,-1]],dtype=np.int32)
     source = 0
 
-    t = bfsv_parallel(graph,source,3)
+    t = bfsv_parallel(nasty_graph,source)
 
 
     
